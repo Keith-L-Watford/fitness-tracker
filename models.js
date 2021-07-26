05f6df9480f7943d87953cd62357d3018d409389
Keith Watford <keith.l.watford@gmail.com>
2021-07-26 16:12:13 -0400

    first draft of model is done, unsure if its formatted correctly

--- models.js
+++ models.js
@@ -2,9 +2,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExcerciseSchema = new Schema({
-    
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    excercises: {
+        type: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            trim: true,
+        },
+        distance: {
+            type: Number,
+            trim: true,
+        },
+        weight: {
+            type: Number,
+            trim: true,
+        },
+        reps: {
+            type: Number,
+            trim: true,
+        },
+        sets: {
+            type: Number,
+            trim: true,
+        },
+    },
 });
 
 const Exercise = mongoose.model("Exercise", ExcerciseSchema)
 
-module.exports = Exercise; 
+module.exports = Exercise;
