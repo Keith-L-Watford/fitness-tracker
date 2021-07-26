6b60cb6664e19dd6737e302007c3afdb7dccb7fc
Keith Watford <keith.l.watford@gmail.com>
2021-07-26 16:02:33 -0400

    wrote out some notes/gameplan

---
+++ server.js
@@ -0,0 +1,15 @@
+// create the server with all the required things
+
+
+
+
+// POST exercise to most recent workout plan
+
+
+// POST new excercise to new workout plan
+
+
+// GET combined weight of multiple exercises from the past seven workouts on the stats page.
+// GET total duration of each workout from the past seven workouts on the stats page.
+
+// APP IS listening on....
