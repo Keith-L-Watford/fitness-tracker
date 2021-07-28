0c739445b2592eb7aaa3eaed67f29c32024f40ff
Keith Watford <keith.l.watford@gmail.com>
2021-07-27 21:24:43 -0400

    added index.html path

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,7 +2,10 @@
 const router = require('express').Router();
 const path = require("path")
 
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, 'exercise.html')));
 router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, 'stats.html')));
 
+// GET combined weight of multiple exercises from the past seven workouts on the stats page.
+// GET total duration of each workout from the past seven workouts on the stats page.
 module.exports = router
