60540f99ea3e87f6585d1b126b17d6ead6c53b18
Keith Watford <keith.l.watford@gmail.com>
2021-07-27 21:32:08 -0400

    finally got the html routes working

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,9 +2,9 @@
 const router = require('express').Router();
 const path = require("path")
 
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, 'exercise.html')));
-router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, 'stats.html')));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '../public/exercise.html')));
+router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '../public/stats.html')));
 
 // GET combined weight of multiple exercises from the past seven workouts on the stats page.
 // GET total duration of each workout from the past seven workouts on the stats page.
