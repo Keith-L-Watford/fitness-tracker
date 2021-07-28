3b7066179e905c84729c82740cab002bfa87edb6
Keith Watford <keith.l.watford@gmail.com>
2021-07-28 19:18:03 -0400

    working on deployment issues

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose")
 
-const apiRoutes = require("./controllers/api-routes.js")
-const htmlRoutes = require("./controllers/html-routes.js")
+const apiRoutes = require("./controllers/api-routes")
+const htmlRoutes = require("./controllers/html-routes")
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,11 +14,19 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.use(apiRoutes)
 app.use(htmlRoutes)
