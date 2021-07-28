01790d3f89fb1864c03cc91b8e4e9d9b8fda1c60
Keith Watford <keith.l.watford@gmail.com>
2021-07-27 20:40:05 -0400

    got help on the server. added app.use. added mongoose.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 // create the server with all the required things
 const express = require("express");
-const mongojs = require("mongojs");
+// const mongojs = require("mongojs");
 const logger = require("morgan");
+const mongoose = require("mongoose")
+
+const apiRoutes = require("./controllers/api-routes.js")
+const htmlRoutes = require("./controllers/html-routes.js")
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -12,22 +18,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const databaseUrl = "fitnessTracker";
-const collections = ["exercises"];
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", { useNewUrlParser: true });
 
-const db = mongojs(databaseUrl, collections);
+app.use(apiRoutes)
+app.use(htmlRoutes)
 
-db.on("error", error => {
-    console.log("Database Error:", error);
-  });
+
+
+// const databaseUrl = "fitnessTracker";
+// const collections = ["exercises"];
+
+// const db = mongojs(databaseUrl, collections);
+
+// db.on("error", error => {
+//     console.log("Database Error:", error);
+//   });
   
-// POST exercise to most recent workout plan
 
-
-// POST new excercise to new workout plan
-
-
-// GET combined weight of multiple exercises from the past seven workouts on the stats page.
-// GET total duration of each workout from the past seven workouts on the stats page.
-
-// APP IS listening on....
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}!`);
+});
