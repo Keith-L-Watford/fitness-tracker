0a65e67e740daf0b4a4fb5d32818ddfb048aac3c
Keith Watford <keith.l.watford@gmail.com>
2021-07-28 20:27:06 -0400

    cleaned file

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -6,4 +6,4 @@ router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index
 router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '../public/exercise.html')));
 router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '../public/stats.html')));
 
-module.exports = router
+module.exports = router;
