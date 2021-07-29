2e9498408ae7afa093598a38a5575dd022f89b08
Keith Watford <keith.l.watford@gmail.com>
2021-07-29 09:46:11 -0400

    fixed a spelling mistake

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -7,7 +7,7 @@ const WorkoutSchema = new Schema({
         default: Date.now
     },
 
-    excercises: [{
+    exercises: [{
         type: {
             type: String,
             required: true,
