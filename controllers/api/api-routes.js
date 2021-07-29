5a0955c23f158ebe3a50a6ff795dd5671582f206
Keith Watford <keith.l.watford@gmail.com>
2021-07-28 22:09:42 -0400

    still unsure what is happening around line 15

--- controllers/api/api-routes.js
+++ controllers/api/api-routes.js
@@ -1,9 +1,7 @@
 // utilize router.
 const router = require('express').Router();
 // create an instance of model
-const {
-  Workout
-} = require('../../models/workoutModel.js');
+const Workout = require('../../models/workoutModel.js');
 
 // ===============================================
 
@@ -11,8 +9,8 @@ const {
 // res = await fetch("/api/workouts");
 // aggregate??
 // Get all workouts
-router.get("/api/workouts", ( req, res) => {
-  Workouts.find({}, (err, workout) => {
+router.get("/api/workouts", (req, res) => {
+  Workout.workouts.find({}, (err, workout) => {
     if (err) {
       res.json(err);
     } else {
@@ -24,16 +22,18 @@ router.get("/api/workouts", ( req, res) => {
 // const res = await fetch("/api/workouts", {
 //   method: "POST",
 // POST new workout(???)
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-      .then((db) => {
-        res.json(db);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-  
+router.post("/api/workouts", ({
+  body
+}, res) => {
+  Workout.create(body)
+    .then((db) => {
+      res.json(db);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 
 
 // async getWorkoutsInRange() {
@@ -41,14 +41,14 @@ router.post("/api/workouts", ({ body }, res) => {
 // agregate??
 // GET workout range
 router.get("/api/workouts/range", (req, res) => {
-//   Workout.find({})
-//     .then(db => {
+  //   Workout.find({})
+  //     .then(db => {
 
-//       res.json(db);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
+  //       res.json(db);
+  //     })
+  //     .catch(err => {
+  //       res.status(400).json(err);
+  //     });
 });
 
 
@@ -57,16 +57,16 @@ router.get("/api/workouts/range", (req, res) => {
 // PUT new exercise
 router.put("/api/workouts/:id", (req, res) => {
   // console.log(req.params.id);
-  Workout.findByIdAndUpdate(
-    { _id:  params.id},
-      {
-        // $inc: { totalDuration: req.body.duration },
-        $push: {
-          exercises: req.body
-        }
-      }, 
-      {new: true}
-      )
+  Workout.findByIdAndUpdate({
+      _id: params.id
+    }, {
+      // $inc: { totalDuration: req.body.duration },
+      $push: {
+        exercises: req.body
+      }
+    }, {
+      new: true
+    })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
